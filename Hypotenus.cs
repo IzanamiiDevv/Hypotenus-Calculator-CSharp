6ec2ed1120ea57294ae0fa9593646223665f9a10
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-24 14:17:08 +0800

    code

---
+++ Hypotenus.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Hypotenus
+{
+    class Hypo
+    {
+        public static int Calc(int x, int y)
+        {
+            Math.Pow(3,3);
+            return x + y;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
 ﻿using System;
+using Hypotenus;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Hypo.Calc(4,6));
+    }
+}
